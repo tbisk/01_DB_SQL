70499347a2626e3ad8d1cb53cbfd614f56554a8c
tbisk <tbisk@web.de>
2021-09-08 10:12:51 +0000

    id / Primary Key

--- 02_SQL_Tables/02c_db_table_SQL.sql
+++ 02_SQL_Tables/02c_db_table_SQL.sql
@@ -11,12 +11,12 @@ DROP DATABASE IF EXISTS boo;
 CREATE DATABASE IF NOT EXISTS boo;
 
 /* DB auswählen */
-USE boo;
+USE boo;  -- oder boo.xxx
 
 /* Tabelle anlegen, falls noch nicht vorhanden */
 CREATE TABLE IF NOT EXISTS test
 (
-    
+    id INT NOT NULL PRIMARY KEY,   
     name VARCHAR(20) NOT NULL DEFAULT "TBA",
     age INT NOT NULL DEFAULT 0
 );
@@ -24,20 +24,18 @@ CREATE TABLE IF NOT EXISTS test
 /* Struktur der Tabelle anzeigen */
 DESCRIBE test;
 
-/* ----- Daten ------- */
-INSERT INTO test(name,age) VALUES ("Grizabella", 29);
-INSERT INTO test(age,name) VALUES (35, "Alonzo");
-INSERT INTO test VALUES ();
+/* ----- Daten ------- 1a: z.B. Index-Einlesen von Tabellen */
+INSERT INTO test(id, name,age) VALUES (1, "Grizabella", 29);
+INSERT INTO test(id, name,age) VALUES (2, "Alonzo", 35);
+INSERT INTO test(id, name,age) VALUES (3, "Alonzo", 24);
+INSERT INTO test(id, name,age) VALUES (4, "Maxine", 26);
 
-/* Doppelte Datensätze werden zugelassen! */
-INSERT INTO test(age,name) VALUES (24, "Alonzo");
+
 
 /* Inhalte der Tabelle anzeigen */
 SELECT * FROM test;
 
-DROP TABLE IF EXISTS test;
 
-SHOW TABLES;
 
 
 
